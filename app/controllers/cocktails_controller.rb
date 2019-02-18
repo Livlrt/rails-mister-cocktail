668a770c39d7fff62c9ea6fd6c1a184059aa3bce
Livia Lortscher <livialortscher@hotmail.com>
2019-02-18 09:03:11 +0100

    update M. Cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -20,6 +20,13 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def upload
+    uploaded_io = params[:cocktail][:picture]
+    File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+      file.write(uploaded_io.read)
+    end
+  end
+
   private
 
   def cocktail_params
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+puts 'Creating ingredients...'
+ingredients['drinks'].each do |element|
+  ingredient = Ingredient.new(name: element['strIngredient1'])
+  ingredient.save!
+end
+puts 'Finished!'
